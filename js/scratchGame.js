49fd187eb349d530df3291aaf038cd8849e1bb1e
Varsha R G <gudigarvarsha3535@gmail.com>
2026-08-08 01:57:37 +0530

    Update scratchGame.js

--- js/scratchGame.js
+++ js/scratchGame.js
@@ -1,10 +1,3 @@
-console.log("SCRATCH GAME JS LOADED");
-
-/* ===========================================
-   SCRATCH & MATCH ENGINE
-=========================================== */
-
-
 const nouns = [
 
     {
